48f81079e76e50b299ad0b73296b1c10bf28718b
songsickk <songsickk@naver.com>
2024-08-01 15:49:17 +0900

    calender view

---
+++ app/src/main/java/com/example/greenswuniex/ChallengeCalenderFragment.kt
@@ -0,0 +1,46 @@
+package com.example.greenswuniex
+
+import android.content.Context
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.fragment.findNavController
+import com.google.android.material.datepicker.DayViewDecorator
+import com.prolificinteractive.materialcalendarview.CalendarDay
+import com.prolificinteractive.materialcalendarview.MaterialCalendarView
+
+class ChallengeCalenderFragment : Fragment() {
+
+
+    lateinit var btncalender: Button
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {//뷰바인딩
+        val view = inflater.inflate(R.layout.fragment_challenge_calender, container, false)
+
+
+
+        //버틑 클릭 시 챌린지 선택으로 이동
+        btncalender = view.findViewById(R.id.btncalender)
+
+        /*btncalender.setOnClickListener {
+            findNavController().navigate(R.id.Challenge_Category)
+
+        }*/
+        return view
+
+
+
+
+
+    }
+
+
+
+}
